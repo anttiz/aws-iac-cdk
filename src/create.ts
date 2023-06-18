3d9820a8391c1e562484348232d073474955c78f
Antti Lehtonen <antti.lehtonen@wunderdog.fi>
2023-06-18 11:43:36 +0300

    Fixes to get userId into table.

--- src/create.ts
+++ src/create.ts
@@ -2,28 +2,33 @@ import * as uuid from "uuid";
 import { Handler } from "aws-lambda";
 import { DynamoDB } from "aws-sdk";
 import { config } from "dotenv";
+import {CognitoIdToken} from '@xeedware/cognito-jwt/dist';
+
 config();
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
 export const handler: Handler = (event, context, callback) => {
   const timeStamp = new Date().getTime();
+  const token = event.headers.Authorization.split(' ')[1];
+  const cognitoIdToken = new CognitoIdToken(token);
   const data = JSON.parse(event.body);
-  if (typeof data.name !== "string") {
+  if (typeof data.name !== "string" || !cognitoIdToken || !cognitoIdToken.sub) {
     console.error("Validation Failed");
     callback(new Error("Couldn't create the todo item."));
     return;
   }
   const id = uuid.v1();
   const params = {
-    TableName: process.env.TODO_TABLE ?? '',
+    TableName: process.env.TODO_TABLE ?? "",
     Item: {
       id,
       todoId: id,
+      userId: cognitoIdToken.sub,
       name: data.name,
       createdAt: timeStamp,
       updatedAt: timeStamp,
-      expiryPeriod: timeStamp
+      expiryPeriod: timeStamp,
     },
   };
 
@@ -41,8 +46,8 @@ export const handler: Handler = (event, context, callback) => {
       statusCode: 200,
       body: JSON.stringify(params.Item),
       headers: {
-        'Access-Control-Allow-Origin': "*",
-        'Access-Control-Allow-Credentials': true,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
       },
     };
     callback(null, response);
